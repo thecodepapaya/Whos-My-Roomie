6b6544a09f4f34a2d1ca18a435bde4745f966cd5
ashutoshsingh05 <201851029@iiitvadodara.ac.in>
2019-05-11 12:00:05 +0530

    First commit

---
+++ android/app/src/main/java/com/example/whos_my_roomie/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.whos_my_roomie;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(LoginPage());
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
