4ce613735b945b6e28c83c3d546ddb238b9c05bb
ashutoshsingh05 <201851029@iiitvadodara.ac.in>
2019-05-14 11:08:55 +0530

    Added input formatters on signUp form

--- lib/Pages/signUp.dart
+++ lib/Pages/signUp.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'dart:core';
 
 class SignUp extends StatefulWidget {
   @override
@@ -13,7 +15,8 @@ class _SignUpState extends State<SignUp> {
   TextEditingController _nameController = TextEditingController();
   TextEditingController _usernameController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
-  TextEditingController _collegeDetailsPageController = TextEditingController();
+  TextEditingController _collegeDetailsNameController = TextEditingController();
+  TextEditingController _collegeDetailsYearController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -56,11 +59,21 @@ class _SignUpState extends State<SignUp> {
                   padding: EdgeInsets.all(40),
                   child: TextFormField(
                     controller: _nameController,
+                    inputFormatters: <TextInputFormatter>[
+                      WhitelistingTextInputFormatter(
+                        RegExp("[a-zA-Z\ ]"),
+                      ),
+                    ],
+                    validator: (String name) {
+                      if (name == "" || name == " ") {
+                        return "Name cannot be omitted!";
+                      } else {
+                        return null;
+                      }
+                    },
                     decoration: InputDecoration(
                       labelText: "Name",
-                      errorText: _nameController.text == ""
-                          ? "Name cannot be empty!"
-                          : null,
+                      //errorText: _nameController.text == ""? "Name cannot be empty!": null,
                     ),
                   ),
                 ),
@@ -80,6 +93,11 @@ class _SignUpState extends State<SignUp> {
                   padding: EdgeInsets.all(40),
                   child: TextFormField(
                     controller: _usernameController,
+                    inputFormatters: <TextInputFormatter>[
+                      WhitelistingTextInputFormatter(
+                        RegExp("[a-zA-Z0-9\_\.\-\@\#\$]"),
+                      ),
+                    ],
                     decoration: InputDecoration(
                       labelText: "Name",
                       errorText: _usernameController.text == ""
@@ -104,6 +122,11 @@ class _SignUpState extends State<SignUp> {
                   padding: EdgeInsets.all(40),
                   child: TextFormField(
                     controller: _passwordController,
+                    inputFormatters: <TextInputFormatter>[
+                      WhitelistingTextInputFormatter(
+                        RegExp("[a-zA-Z0-9\!\@\#\$\%\&\*\_\.\+\-\?]"),
+                      ),
+                    ],
                     decoration: InputDecoration(
                       labelText: "Password",
                       errorText: _usernameController.text == ""
@@ -119,24 +142,66 @@ class _SignUpState extends State<SignUp> {
         ),
 //############################################ CollegeDetailsPage ############################################################
         Scaffold(
-          body: Center(
-            child: Column(
-              children: <Widget>[
-                Image.asset("assets/icon.png"),
-                Text("Set your password"),
-                Padding(
-                  padding: EdgeInsets.all(40),
-                  child: TextFormField(
-                    controller: _collegeDetailsPageController,
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      errorText: _usernameController.text == ""
-                          ? "Password cannot be empty!"
-                          : null,
+          body: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Image.asset("assets/icon.png"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Enter your",
+                      ),
+                      Text(
+                        " College ",
+                        style: TextStyle(
+                          decoration: TextDecoration.lineThrough,
+                        ),
+                      ),
+                      Text(
+                        "Institute",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(" Details")
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(40),
+                    child: TextFormField(
+                      controller: _collegeDetailsNameController,
+                      decoration: InputDecoration(
+                        labelText: "Isntitute Name",
+                        errorText: _usernameController.text == ""
+                            ? "Institute name cannot be empty!"
+                            : null,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: EdgeInsets.all(40),
+                    child: TextFormField(
+                      controller: _collegeDetailsYearController,
+                      inputFormatters: [
+                        WhitelistingTextInputFormatter(RegExp("[0-9]")),
+                      ],
+                      validator: (String _year) {
+                        if (_year.length != 4) {
+                          return "Invalid Graduation Year";
+                        } else {
+                          return null;
+                        }
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Graduation Year",
+                        //errorText: _usernameController.text == ""? "Graduation Year cannot be empty!": null,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           persistentFooterButtons: persistentFooterButtonsBuilder(4),
